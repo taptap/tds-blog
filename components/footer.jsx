63f407f39e57968de9bbef809b6f262721100acc
Fuchen Shi <shifuchen@xd.com>
2021-12-06 14:58:50 +0800

    add spacing between columns in footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -12,92 +12,96 @@ export default function Footer() {
             <section>
               <h2>关注我们</h2>
 
-              <div className={styles.list}>
-                <ul className={styles.small}>
-                  <li>
-                    <a
-                      href="https://taptap.com"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <h3>TapTap 中国大陆版</h3>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://taptap.io"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <h3>TapTap 国际版</h3>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://developer.taptap.com"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <h3>开发者中心</h3>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className={styles.list}>
-                <ul className={styles.small}>
-                  <li>
-                    <a
-                      href="https://twitter.com/taptap"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <h3>@TapTap</h3>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://reddit.com/r/taptap"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <h3>r/TapTap</h3>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://weibo.com/taptapgames"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <h3>TapTap 官方微博</h3>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className={styles.list}>
-                <ul className={styles.small}>
-                  <li>
-                    <a href="/rss.xml">
-                      <h3>RSS Feed</h3>
-                    </a>
-                  </li>
-                  <li>
-                    <Link href="/open">
-                      <a>
-                        <h3>开放资源</h3>
+              <div className={styles.lists}>
+                <div className={styles.list}>
+                  <ul className={styles.small}>
+                    <li>
+                      <a
+                        href="https://taptap.com"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <h3>TapTap 中国大陆版</h3>
                       </a>
-                    </Link>
-                  </li>
-                  <li>
-                    <a
-                      href="https://career.taptap.dev"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <h3>加入我们</h3>
-                    </a>
-                  </li>
-                </ul>
+                    </li>
+                    <li>
+                      <a
+                        href="https://taptap.io"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <h3>TapTap 国际版</h3>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://developer.taptap.com"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <h3>开发者中心</h3>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className={styles.list}>
+                  <ul className={styles.small}>
+                    <li>
+                      <a
+                        href="https://twitter.com/taptap"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <h3>@TapTap</h3>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://reddit.com/r/taptap"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <h3>r/TapTap</h3>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://weibo.com/taptapgames"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <h3>TapTap 官方微博</h3>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className={styles.list}>
+                  <ul className={styles.small}>
+                    <li>
+                      <a href="/rss.xml">
+                        <h3>RSS Feed</h3>
+                      </a>
+                    </li>
+                    <li>
+                      <Link href="/open">
+                        <a>
+                          <h3>开放资源</h3>
+                        </a>
+                      </Link>
+                    </li>
+                    <li>
+                      <a
+                        href="https://career.taptap.dev"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <h3>加入我们</h3>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </section>
           </div>
