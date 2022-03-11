d7212a3d1e5ba7a01a8bdc207f1cd3507041edb3
Jang Rush <weakish@gmail.com>
2022-03-11 20:57:37 +0800

    fix: image optimization for next export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const nextConfig = {
   reactStrictMode: true,
   // https://github.com/vercel/next.js/issues/3168#issuecomment-340166440
   webpack: (config, { buildId, dev }) => {
     config.resolve.symlinks = false
     return config
-  }
+  },
 }
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }],
+
+  // your other plugins here
+
+], nextConfig);
