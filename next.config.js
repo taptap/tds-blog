34c9e49ad648af6b3b2d3623e9d3541d8416a539
Jang Rush <weakish@gmail.com>
2022-03-11 20:10:04 +0800

    fix: work around with next.js symlink issue

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // https://github.com/vercel/next.js/issues/3168#issuecomment-340166440
+  webpack: (config, { buildId, dev }) => {
+    config.resolve.symlinks = false
+    return config
+  }
 }
